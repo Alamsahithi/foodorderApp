2b03aa4ae931b21544950f97d8c38c8ab7aa6df9
Alamsahithi <sahithialam13@gmail.com>
2023-11-21 16:19:17 +0530

    till mealsList

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from './MealsSummary.module.css'
+const MealsSummary = () =>{
+    return (
+    <section className={classes.summary}>
+        <h2>delicious food ,delivered to u</h2>
+        <p>choose ur fav meal</p>
+        <p>all our meals are cooked with high quality ingredients</p>
+    </section>
+    )
+}
+
+export default MealsSummary
