32be8ef4a388cc3e583bc23dacfa5e5a1509e8c7
Alamsahithi <sahithialam13@gmail.com>
2023-11-22 12:31:14 +0530

    closing the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,31 +1,30 @@
-import React from "react";
-import styles from "./Cart.module.css";
-import Modal from "../UI/Modal/Modal";
+import classes from './Cart.module.css'
+import Modal from '../UI/Modal'
 
-const Cart = () => {
-  return (
-    <Modal
-      open={true}
-      isClosable={true}
-      title={"Cart Items"}
-      footer={[
-        <div key={"footer"} className={styles.cart_modal_footer}>
-          <button key={"close"} className={styles.close_btn}>
-            Close
-          </button>
-          <button key={"order"} className={styles.order_btn}>
-            Order
-          </button>
-        </div>,
-      ]}
-    >
-      <div>
-        Sushi
-        <p>Total Amount</p>
-        <p>35.62</p>
-      </div>
-    </Modal>
-  );
-};
+const Cart = props => {
+    const cartItems=(
+        <ul className={classes['cart-items']}>
+            {[{id:'c1',name:'sushi',amount:2,price: 12}].map(item=>(
+              <li>{item.name}</li>
+            ))}
 
-export default Cart;
+        </ul>
+    )
+    return (
+        <Modal onClose={props.onClose}>
+            {cartItems}
+            <div className={classes.total}>
+                <span>total amount</span>
+                <span>35.62</span>
+           </div>
+           <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+            <button className={classes.button}>Order</button>
+           </div>
+        </Modal>
+    
+    
+    )
+}
+
+export default Cart
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,23 +1,25 @@
-import React from "react";
-import styles from "./Modal.module.css";
-import { CloseOutlined } from "@ant-design/icons";
-
-const Modal = ({ open, title, isClosable, children, footer }) => {
-  return (
-    <div
-      className={styles.modal_wrapper}
-      style={{ display: open ? "grid" : "none" }}
-    >
-      <div className={styles.modal}>
-        <div className={styles.modal_header}>
-          <p>{title}</p>
-          {isClosable === false ? <></> : <CloseOutlined />}
-        </div>
-        <div className={styles.modal_body}>{children}</div>
-        <div className={styles.modal_footer}>{footer}</div>
-      </div>
+import classes from './Modal.module.css'
+import { Fragment } from 'react'
+import { ReactDOM } from 'react-dom'
+const Backdrop = (props)=>{
+   return <div className={classes.Backdrop} onClick={props.onClose}/>
+}
+const ModalOverlay=(props)=>{
+    return (
+    <div className={classes.modal}>
+    <div className={classes.content}>{props.children}</div>
     </div>
-  );
-};
+    )
+}
 
-export default Modal;
+const portalElement = document.getElementById('overlays')
+
+const Modal = (props )=>{
+    return <Fragment>
+        {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>,portalElement)}
+        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
+
+    </Fragment>
+   
+}
+export default Modal
