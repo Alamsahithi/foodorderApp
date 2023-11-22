a855160fdc0de0a350424aecef1d0ad0be50beb1
Alamsahithi <sahithialam13@gmail.com>
2023-11-22 11:30:53 +0530

    Making a hard Coded Cart without any Functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,31 @@
-import classes from './Cart.module.css'
+import React from "react";
+import styles from "./Cart.module.css";
+import Modal from "../UI/Modal/Modal";
 
-const Cart = props => {
-    return <div>
-        cartitems</div>
-    <div></div>
-}
+const Cart = () => {
+  return (
+    <Modal
+      open={true}
+      isClosable={true}
+      title={"Cart Items"}
+      footer={[
+        <div key={"footer"} className={styles.cart_modal_footer}>
+          <button key={"close"} className={styles.close_btn}>
+            Close
+          </button>
+          <button key={"order"} className={styles.order_btn}>
+            Order
+          </button>
+        </div>,
+      ]}
+    >
+      <div>
+        Sushi
+        <p>Total Amount</p>
+        <p>35.62</p>
+      </div>
+    </Modal>
+  );
+};
 
-export default Cart
+export default Cart;
