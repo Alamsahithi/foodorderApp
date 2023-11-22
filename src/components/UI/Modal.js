9d97b8916c0daf0c159a53c626f733639ce8c1c9
Alamsahithi <sahithialam13@gmail.com>
2023-11-22 14:39:44 +0530

    implementing cart items

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,6 +1,7 @@
 import classes from './Modal.module.css'
 import { Fragment } from 'react'
-import { ReactDOM } from 'react-dom'
+import React from 'react'
+//import { ReactDOM } from 'react-dom'
 const Backdrop = (props)=>{
    return <div className={classes.Backdrop} onClick={props.onClose}/>
 }
@@ -12,12 +13,12 @@ const ModalOverlay=(props)=>{
     )
 }
 
-const portalElement = document.getElementById('overlays')
+//const portalElement = document.getElementById('overlays')
 
 const Modal = (props )=>{
     return <Fragment>
-        {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>,portalElement)}
-        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
+    <Backdrop onClose={props.onClose}/>
+    <ModalOverlay>{props.children}</ModalOverlay>
 
     </Fragment>
    
